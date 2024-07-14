f576ce9c2ce53f2ba261ff6b4f201fac34dc2c46
muhib bakar <muhib.bakar@Cdocs.local>
2024-07-14 16:26:38 +0500

    Day 2 - Operators

---
+++ Day2 - Operators/day2.js
@@ -0,0 +1,28 @@
+// Activity 1 - Arithmetic Operators
+
+const num1 = 2;
+const num2 = 3;
+console.log("Additon: ", num1 + num2);
+console.log("Subtraction: ", num1 - num2);
+console.log("Multiplication: ", num1 * num2);
+console.log("Division: ", num1 / num2);
+console.log("Reminder: ", num1 % num2);
+
+// Activity 2 - Assignment Operators
+let num3 = 2;
+console.log((num3 += 1));
+console.log((num3 -= 1));
+
+// Activity 3 - Comparison Operators
+console.log(num1 > num2, num1 < num2);
+console.log(num1 >= num2, num1 <= num2);
+console.log(num1 == num2, num1 === num2);
+
+// Activity 4 - Logical Operators
+console.log(num1 || num2);
+console.log(num1 && num2);
+console.log(!true, !false);
+
+// Activity 5 - Ternary Operator
+console.log(true ? num1 : num2);
+console.log(num1 > 0 ? "Positive" : "Negative");
